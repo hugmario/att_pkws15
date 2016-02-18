7f4489657875d574ebc8cffae3a14deb8234dc10
Daniel Bigl <danibigl@hotmail.com>
2016-02-18 19:14:25 +0100

    Testing Merge

--- project/src/AllThoseTerritories.java
+++ project/src/AllThoseTerritories.java
@@ -5,6 +5,7 @@
 import javax.swing.*;
 // ein weiteres kommentar :D
 // testing  GitHub
+// testing merge
 public class AllThoseTerritories {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
